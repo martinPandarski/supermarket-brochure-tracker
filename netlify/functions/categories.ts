18933751b958db70bbf2313b35a215958fb58eca
Nikol Nikolova <nikolnikolova@Nikols-MacBook-Air.local>
2026-01-29 21:06:11 +0200

    feat: add netlify functions

---
+++ netlify/functions/categories.ts
@@ -0,0 +1,30 @@
+import type { Handler } from "@netlify/functions"
+import axios from "axios"
+
+export const handler: Handler = async (event) => {
+  try {
+    const response = await axios.get(
+      `${process.env.VITE_API_BASE_URL}categories`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.VITE_API_KEY}`,
+        },
+      }
+    )
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(response.data),
+    }
+  } catch (error: any) {
+    return {
+      statusCode: error.response?.status || 500,
+      body: JSON.stringify({
+        message: "Failed to fetch products",
+      }),
+    }
+  }
+}
